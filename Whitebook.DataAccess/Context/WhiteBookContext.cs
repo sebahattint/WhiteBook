07f4d07e5b28cd08a48b530da3f49d84934b7741
yasinozkaynk <77541257+yasinozkaynk@users.noreply.github.com>
2021-10-16 17:30:53 +0300

    .

--- Whitebook.DataAccess/Context/WhiteBookContext.cs
+++ Whitebook.DataAccess/Context/WhiteBookContext.cs
@@ -7,9 +7,6 @@ namespace Whitebook.DataAccess.Context
 {
     public class WhiteBookContext:DbContext
     {
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer(@"server = (localdb)\mssqllocaldb; initial catalog =Northwind; integrated security = true");
-        }
+
     }
 }
